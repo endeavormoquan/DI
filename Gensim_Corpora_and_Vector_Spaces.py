67df4295e7484601e9fb56bb1dc9033ad4ad5499
Colony Albert <d1299026676d@126.com>
2017-09-25 19:52:01 +0800

    test the gensim package in English and in Chinese.

---
+++ Gensim_Corpora_and_Vector_Spaces.py
@@ -0,0 +1,60 @@
+def testGensim():
+    import logging
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    import os
+    import tempfile
+    # TEMP_FOLDER = tempfile.gettempdir()
+    TEMP_FOLDER = 'D:\Git\DI'
+    print(TEMP_FOLDER)
+    print('Folder "{}" will be used to save temporary dictionary and corpus.'.format(TEMP_FOLDER))
+
+    from gensim import corpora
+
+    documents = ["Human machine interface for lab abc computer applications",
+                 "A survey of user opinion of computer system response time",
+                 "The EPS user interface management system",
+                 "System and human system engineering testing of EPS",
+                 "Relation of user perceived response time to error measurement",
+                 "The generation of random binary unordered trees",
+                 "The intersection graph of paths in trees",
+                 "Graph minors IV Widths of trees and well quasi ordering",
+                 "Graph minors A survey"]
+
+    # remove common words and tokenize
+    stoplist = set('for a of the and to in'.split())
+    texts = [[word for word in document.lower().split() if word not in stoplist]
+             for document in documents]
+
+    # remove words that appear only once
+    from collections import defaultdict
+    frequency = defaultdict(int)
+    for text in texts:
+        for token in text:
+            frequency[token] += 1
+
+    texts = [[token for token in text if frequency[token] > 1] for text in texts]
+
+    from pprint import pprint  # pretty-printer
+    pprint(texts)
+
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save(os.path.join(TEMP_FOLDER, 'deerwester.dict'))  # store the dictionary, for future reference
+    print(dictionary)
+
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save(os.path.join(TEMP_FOLDER,'deerwester.dict'))
+    print(dictionary)
+
+    print(dictionary.token2id)
+    new_doc = 'human computer interaction'
+    new_vec = dictionary.doc2bow(new_doc.lower().split())
+    print(new_vec)
+
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    corpora.MmCorpus.serialize(os.path.join(TEMP_FOLDER,'deerwester.mm'),corpus)
+    for c in corpus:
+        print(c)
+
+if __name__ == '__main__':
+    testGensim()
---
+++ Gensim_Topics_and_Transformations.py
@@ -0,0 +1,12 @@
+import gensim
+import jieba
+def Topics_and_Transformations():
+    documents = ['我是丁启民','我来自西北工业大学','我今年21岁了']
+    texts = [[word for word in list(jieba.cut(document))] for document in documents]
+    model = gensim.models.Word2Vec(texts, size=10, workers=1, min_count=1)
+    model.save('D:\Git\DI\model')
+    # model = gensim.models.Word2Vec.load('D:\Git\DI\model')
+    print(model.wv['丁启民'])
+
+if __name__ == '__main__':
+    Topics_and_Transformations()
